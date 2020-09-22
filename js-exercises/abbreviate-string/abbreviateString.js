0ed7110f702871b3255bf501ff75b1a091751d98
Gaurab Majumdar <gaurav.majumdar10@gmail.com>
2020-09-22 17:00:26 +0530

    Solved Abbreviated String exercise

--- js-exercises/abbreviate-string/abbreviateString.js
+++ js-exercises/abbreviate-string/abbreviateString.js
@@ -1,5 +1,16 @@
 import { isString } from "util";
 
-function abbreviateString(str) {}
+function abbreviateString(str) {
+
+    const inputNameArr = str.split(" ");
+
+
+    if(inputNameArr.length > 1){
+       var abbrString=inputNameArr[0] + " " + inputNameArr[inputNameArr.length - 1].charAt(0).toUpperCase() + ".";
+    }
+
+    return abbrString;
+
+}
 
 export { abbreviateString };
