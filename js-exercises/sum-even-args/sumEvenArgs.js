9658cc30f8d7207a4d85ff274b2bc3b9c383ecec
Gaurab Majumdar <gaurav.majumdar10@gmail.com>
2020-09-23 18:21:33 +0530

    Solved Sum Even Arguments Exercise

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,5 +1,13 @@
 const sumEvenArgs = (...args) => {
-  return args;
+
+  let sum=0;
+  for(let i of args){
+    if(i%2==0){
+      sum+=i;
+    }
+  }
+
+  return sum;
 };
 
 export { sumEvenArgs };
